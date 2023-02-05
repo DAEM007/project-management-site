95a3158bca64c6a43e47a43dd8aab613b8fff9a4
EMMANUEL <dammylola299@gmail.com>
2023-02-05 20:02:09 +0100

    Renaming of file and route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 // All pages import
 import Dashboard from "./pages/dashboard/Dashboard";
-import ProjectDetails from "./pages/project/ProjectDetails";
+import Project from "./pages/project/Project";
 import Create from "./pages/create/Create";
 import Login from "./pages/login/Login";
 import Signup from './pages/signup/Signup';
@@ -16,7 +16,7 @@ function App() {
         <Router>
           <Routes>
             <Route path='/' element={ <Dashboard /> } />
-            <Route path='/projectDetails/:id' element={ <ProjectDetails /> } />
+            <Route path='/project/:id' element={<Project />} />
             <Route path='/create' element={ <Create /> } />
             <Route path='/login' element={ <Login /> } />
             <Route path='/signup' element={ <Signup /> } />
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,5 @@
 // All styles
-import "./ProjectDetails.css";
+import "./Project.css";
 
 const ProjectDetails = () => {
 
