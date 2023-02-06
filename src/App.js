9427f50c6c039598a401a8844cdd124c3e6ebb11
EMMANUEL <dammylola299@gmail.com>
2023-02-06 06:49:53 +0100

    Added the NavBar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // All styles import
 import './App.css';
+// All components import
+import NavBar from './components/NavBar';
 // All pages import
 import Dashboard from "./pages/dashboard/Dashboard";
 import Project from "./pages/project/Project";
@@ -14,13 +16,16 @@ function App() {
   return (
     <div className="App">
         <Router>
-          <Routes>
-            <Route path='/' element={ <Dashboard /> } />
-            <Route path='/project/:id' element={<Project />} />
-            <Route path='/create' element={ <Create /> } />
-            <Route path='/login' element={ <Login /> } />
-            <Route path='/signup' element={ <Signup /> } />
-          </Routes>
+          <div className="container">
+            <NavBar />
+            <Routes>
+              <Route path='/' element={ <Dashboard /> } />
+              <Route path='/project/:id' element={<Project />} />
+              <Route path='/create' element={ <Create /> } />
+              <Route path='/login' element={ <Login /> } />
+              <Route path='/signup' element={ <Signup /> } />
+            </Routes>
+          </div>
         </Router>
     </div>
   );
