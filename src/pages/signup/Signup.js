3fc3bfd717b0ace58cacbb70b9ef840b8f6de594
EMMANUEL <dammylola299@gmail.com>
2023-02-08 06:31:41 +0100

    Created Signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,50 @@
+// All react imports
+import { useState } from "react";
 // All styles
 import "./Signup.css";
 
 const Signup = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [thumbnail, setThumbnail] = useState(null);
+
     return (
-        <div>Signup</div>
+        <form className="auth-form">
+            <h2>Sign up here...</h2>
+            <label>
+                <input 
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required 
+                />
+            </label>
+            <label>
+                <input 
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required 
+                />
+            </label>
+            <label>
+                <input 
+                    type="name"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    required 
+                />
+            </label>
+            <label>
+                <input 
+                    type="file"
+                    required 
+                />
+            </label>
+            <button className="btn">Sign up</button>
+        </form>
     );
 }
- 
+
 export default Signup;
