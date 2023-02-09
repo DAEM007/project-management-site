43fd8624811b30c0c3994243488b000e7c6d27e1
EMMANUEL <dammylola299@gmail.com>
2023-02-09 06:47:27 +0100

    working on the thumbnail inputs

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,11 +8,35 @@ const Signup = () => {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
     const [thumbnail, setThumbnail] = useState(null);
+    const [thumbnailError, setThumbnailError] = useState(null);
+
+
+    const handleThumbnail = (e) => {
+        setThumbnailError(null);
+        let selected = e.target.files[0];
+
+        console.log(selected);
+
+        if(!selected.type.includes('image')){
+            setThumbnailError('Please select an Image file');
+            return;
+        }
+        if(!selected){
+            setThumbnailError('Please select a file');
+            return;
+        }
+
+        
+        
+    }
+
+    console.log(thumbnailError);
 
     return (
         <form className="auth-form">
             <h2>Sign up here...</h2>
             <label>
+                <span>email:</span>
                 <input 
                     type="email"
                     onChange={(e) => setEmail(e.target.value)}
@@ -21,6 +45,7 @@ const Signup = () => {
                 />
             </label>
             <label>
+                <span>password:</span>
                 <input 
                     type="password"
                     onChange={(e) => setPassword(e.target.value)}
@@ -29,6 +54,7 @@ const Signup = () => {
                 />
             </label>
             <label>
+                <span>name:</span>
                 <input 
                     type="name"
                     onChange={(e) => setName(e.target.value)}
@@ -37,8 +63,10 @@ const Signup = () => {
                 />
             </label>
             <label>
+                <span>thumbnail:</span>
                 <input 
                     type="file"
+                    onChange={handleThumbnail}
                     required 
                 />
             </label>
