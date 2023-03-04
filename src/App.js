96428ec3630c571f19e58ea0c18494a4f2f8d8ca
Emmanuel <dammylola299@gmail.com>
2023-03-04 10:09:41 +0100

    gaurded the routes and redirected users to proper links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // All react imports
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 // All styles import
 import './App.css';
 // All components import
 import NavBar from './components/NavBar';
 import SideBar from './components/SideBar';
+// All hooks import
+import { useAuthContext } from './hooks/useAuthContext';
 // All pages import
 import Dashboard from "./pages/dashboard/Dashboard";
 import Project from "./pages/project/Project";
@@ -13,21 +15,40 @@ import Login from "./pages/login/Login";
 import Signup from './pages/signup/Signup';
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
+      { authIsReady && (
         <Router>
           <SideBar />
           <div className="container">
             <NavBar />
             <Routes>
-              <Route path='/' element={ <Dashboard /> } />
-              <Route path='/project/:id' element={<Project />} />
-              <Route path='/create' element={ <Create /> } />
-              <Route path='/login' element={ <Login /> } />
-              <Route path='/signup' element={ <Signup /> } />
+              <Route 
+                path='/' 
+                element={ user ? <Dashboard /> : <Navigate to="/login" /> } 
+              />
+              <Route 
+                path='/project/:id'
+                element={ user ? <Project /> : <Navigate to="/login" /> } 
+              />
+              <Route 
+                path='/create'
+                element={ user ? <Create /> : <Navigate to="/login" /> } 
+              />
+              <Route 
+                path='/login' 
+                element={ !user ? <Login /> : <Navigate to="/" /> }
+              />
+              <Route 
+                path='/signup'
+                element={ !user ? <Signup /> : <Navigate to="/" /> } 
+              />
             </Routes>
           </div>
         </Router>
+      )}
     </div>
   );
 }
