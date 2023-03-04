c38184551d88f934ca52537304e0c98633c9b28e
Emmanuel <dammylola299@gmail.com>
2023-03-04 20:59:39 +0100

    conditionally rendered sidebar

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
       { authIsReady && (
         <Router>
-          <SideBar />
+          { user && <SideBar /> }
           <div className="container">
             <NavBar />
             <Routes>
