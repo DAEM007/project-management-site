d5e2f27c7ea0b336c4b01d0fc8e43e66b9a4413c
Emmanuel <dammylola299@gmail.com>
2023-03-06 14:19:54 +0100

    added isPending state

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -8,45 +8,50 @@ import { collection, onSnapshot, orderBy, query, where } from "firebase/firestor
 const useCollection = (col, _query, _orderBy) => {
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(false);
     // query and orderBy arguments in the useCollection hook
     const q = useRef(_query).current;
     const order = useRef(_orderBy).current;
 
     useEffect(() => {
+        // start fetch
+        setIsPending(true);
         // collection reference
         let colRef = collection(db, col);
 
         // check for a query argument
-        if(query) {
+        if(q) {
             colRef = query(colRef, where(...q));
         }
 
         // check for a orderBy argument
-        if(orderBy) {
+        if(order) {
             colRef = query(colRef, orderBy(...order));
         }
 
         // Fetch documents from the collection
         const unsub = onSnapshot(colRef, (snapshot) => {
-            const results = [];
+            let results = [];
             snapshot.docs.forEach((doc) => {
                 results.push({ ...doc.data(), id: doc.id })
-            })
+            });
             // console.log(results);
 
             // update states
             setDocuments(results);
             setError(null);
+            setIsPending(false);
         }, (err) => {
             console.log(err.message);
             setError('Could not fetch data from firebase collection');
+            setIsPending(false);
         })
 
         return () => unsub()
         
     }, [col, q, order])
 
-    return { documents, error };
+    return { documents, error, isPending };
 
 }
  
