757f87ce9e7361c7811504af1fd5ef97feb3d491
Emmanuel <dammylola299@gmail.com>
2023-03-27 11:22:59 +0100

    created the project category and assigned users

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,17 +1,44 @@
 // All react imports
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Select from "react-select";
+// All hooks imports
+import useCollection from "../../hooks/useCollection";
 // All styles
 import "./Create.css";
 
+// options for categories
+const categories = [
+    {value: 'development', label: 'Development'},
+    {value: 'design', label: 'Design'},
+    {value: 'sales', label: 'Sales'},
+    {value: 'marketing', label: 'Marketing'},
+]
+
 const Create = () => {
+    // options for the assigned users
+    const { documents } = useCollection('users');
+    const [users, setUsers] = useState([]);
+    // form states
     const [name, setName] = useState('');
     const [details, setDetails] = useState('');
     const [dueDate, setDueDate] = useState('');
+    const [category, setCategory] = useState('');
+    const [assignedUsers, setAssignedUsers] = useState([]);
+
+    // map through the documents from the users collection and return a new array of objects
+    useEffect(() => {
+        if(documents) {
+            const options = documents.map((user) => {
+                return { value: user, label: user.displayName }
+            })
+            setUsers(options)
+        }
+    }, [documents])
 
     // submit form
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, details, dueDate);
+        console.log(name, details, dueDate, category.value, assignedUsers);
     }
 
     return (
@@ -43,6 +70,21 @@ const Create = () => {
                         type="date"
                         onChange={(e) => setDueDate(e.target.value)}
                         value={dueDate}
+                    />
+                </label>
+                <label>
+                    <span>Project Category: </span>
+                    <Select 
+                        onChange={(option) => setCategory(option)}
+                        options={categories}
+                    />
+                </label>
+                <label>
+                    <span>Assigned users: </span>
+                    <Select 
+                        onChange={(option) => setAssignedUsers(option)}
+                        options={users}
+                        isMulti
                     />
                 </label>
                 <button className="btn">Add Project</button>
