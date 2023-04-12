d09f9ae9f0436c8b69647c09cf765e9fad2f3a10
Emmanuel <dammylola299@gmail.com>
2023-04-12 02:19:41 +0100

    updated the dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,17 @@
 // All styles
+import ProjectList from "../../components/ProjectList";
+import useCollection from "../../hooks/useCollection";
 import "./Dashboard.css";
 
 const Dashboard = () => {
+    const {documents, error} = useCollection('projects');
+
     return (
-        <div>Dashboard</div>
+        <div>
+            <h2 className="project-title">Dashboard</h2>
+            { error && <p className="error">{error}</p> }
+            { documents && <ProjectList projects={documents} /> }
+        </div>
     );
 }
  
