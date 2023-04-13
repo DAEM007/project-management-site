ba68db65545885e4ed88ae05e36d18278ec93d20
Emmanuel <dammylola299@gmail.com>
2023-04-13 16:42:55 +0100

    created a project component

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,10 +1,22 @@
+// All react imports
+import { useParams } from "react-router-dom";
 // All styles
 import "./Project.css";
+// All hooks import
+import useDocument from "../../hooks/useDocument";
+// All components imports
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectDetails = () => {
+    const { id } = useParams();
+    const { document, error } = useDocument('projects', id);
 
     return (
-        <div>Project Details</div> 
+        <div className="project-details">
+            { error && <div className="error">{error}</div> }
+            { !document && <div className="loading">Loading...</div> }
+            { document && <ProjectSummary project={document} /> }
+        </div> 
     );
 }
  
