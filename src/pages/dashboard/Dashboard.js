54507f9813e538ef9a0952e3b598d497b948dcaa
Emmanuel <dammylola299@gmail.com>
2023-04-14 16:51:28 +0100

    implemented filter for the dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,16 +1,57 @@
+// All react imports
+import { useState } from "react";
 // All styles
-import ProjectList from "../../components/ProjectList";
-import useCollection from "../../hooks/useCollection";
 import "./Dashboard.css";
+// All component imports
+import ProjectList from "../../components/ProjectList";
+import ProjectFilter from "./ProjectFilter";
+// All hooks import
+import useCollection from "../../hooks/useCollection";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 
 const Dashboard = () => {
+    const { user } = useAuthContext();
     const {documents, error} = useCollection('projects');
+    const [filter, setFilter] = useState('all');
+
+    const changeFilter = (newFilter) => {
+        setFilter(newFilter)
+    }
+
+    const projects = documents ? documents.filter(document => {
+        switch(filter) {
+          case 'all':
+            return true
+          case 'mine':
+            let assignedToMe = false
+            document.assignedUserList.forEach(u => {
+              if(u.id === user.uid) {
+                assignedToMe = true
+              }
+            })
+            return assignedToMe
+          case 'development':
+          case 'design':
+          case 'sales':
+          case 'marketing':
+            console.log(document.category, filter)
+            return document.category === filter
+          default:
+            return true
+        }
+      }) : null
 
     return (
         <div>
             <h2 className="project-title">Dashboard</h2>
             { error && <p className="error">{error}</p> }
-            { documents && <ProjectList projects={documents} /> }
+            { documents && 
+                (<ProjectFilter 
+                    currentFilter={filter}
+                    changeFilter={changeFilter}
+                />) }
+            {projects && <ProjectList projects={projects} />}
         </div>
     );
 }
