7b037aa8cacdeb05c5979a1d8691cf47100e71fc
DAMILOLA <dammylola299@gmail.com>
2024-11-07 19:13:38 +0100

    ts-migrate/hooks

--- src/hooks/useAuthContext.ts
+++ src/hooks/useAuthContext.ts
@@ -1,15 +1,12 @@
-// All react imports
 import { useContext } from "react";
-// context imports
 import { AuthContext } from "../context/AuthContext";
 
 export const useAuthContext = () => {
-    const context = useContext(AuthContext);
+  const context = useContext(AuthContext);
 
-    if(!context){
-        throw new Error('useContext must be inside AuthContextProvider');
-    }
+  if (!context) {
+    throw new Error("useContext must be inside AuthContextProvider");
+  }
 
-    return context;
-}
-
+  return context;
+};
